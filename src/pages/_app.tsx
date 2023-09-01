917fe31459b43478bc5951e789a692970e27b515
NIHILncunia <nihil_ncunia@naver.com>
2023-09-02 00:08:32 +0900

    2023 0902 네이버 검색도구 추가

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -17,6 +17,10 @@ const App = ({ Component, pageProps, }: AppProps) => (
         name='viewport'
         content='width=device-width, initial-scale=1.0'
       />
+      <meta
+        name='naver-site-verification'
+        content='adb852945c57740b1e19ceafc8bdfa930e672f54'
+      />
     </Head>
     <Script
       strategy='afterInteractive'
@@ -25,6 +29,7 @@ const App = ({ Component, pageProps, }: AppProps) => (
 
     <Script
       async
+      strategy='afterInteractive'
       src='https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-9256396675875954'
       crossOrigin='anonymous'
     />
